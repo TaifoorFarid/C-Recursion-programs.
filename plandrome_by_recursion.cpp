a956737cea44d04801999a1f83f01cfc7648d3ee
TaifoorFarid <136922765+TaifoorFarid@users.noreply.github.com>
2023-07-28 05:31:12 -0400

    Update plandrome_by_recursion.cpp

--- plandrome_by_recursion.cpp
+++ plandrome_by_recursion.cpp
@@ -4,6 +4,7 @@ int plndrm(int num, int pln)
 {
 	if(num == 0)
 	return pln;
+	
 	int dig = num % 10;
 	pln = pln * 10 + dig;
 	return plndrm(num / 10, pln);
@@ -14,8 +15,10 @@ main()
 	int num;
 	printf("Enter a number: ");
 	scanf("%d", &num);
+	
 	int pln = 0;
 	pln = plndrm(num, pln);
+	
 	if(pln == num)
 	printf("The number is Plndrom");
 	else
