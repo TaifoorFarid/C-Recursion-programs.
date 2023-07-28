a6b459e77135c86656cb7fa7c4a9853da6dc67cd
TaifoorFarid <136922765+TaifoorFarid@users.noreply.github.com>
2023-07-28 14:29:54 +0500

    Add files via upload
    The program will find weather the num is plandrome or not by recursion.

---
+++ plandrome_by_recursion.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int plndrm(int num, int pln)
+{
+	if(num == 0)
+	return pln;
+	int dig = num % 10;
+	pln = pln * 10 + dig;
+	return plndrm(num / 10, pln);
+}
+
+main()
+{
+	int num;
+	printf("Enter a number: ");
+	scanf("%d", &num);
+	int pln = 0;
+	pln = plndrm(num, pln);
+	if(pln == num)
+	printf("The number is Plndrom");
+	else
+	printf("The number is not plndrom");
+}
